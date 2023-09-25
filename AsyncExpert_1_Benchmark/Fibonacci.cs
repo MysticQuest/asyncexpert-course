e62bdd777a45ca92b4bf35104fd17b79d4af85a5
Mystic Quest <kirimns@gmail.com>
2023-09-25 18:55:21 +0300

    initial commit benchmarking

---
+++ AsyncExpert_1_Benchmark/Fibonacci.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace AsyncExpert_1_Benchmark
+{
+    [DisassemblyDiagnoser(exportCombinedDisassemblyReport: true)]
+    public class FibonacciCalc
+    {
+        // Objectives:
+        // 1. Write implementations for RecursiveWithMemoization and Iterative solutions
+        // 2. Add MemoryDiagnoser to the benchmark
+        // 3. Run with release configuration and compare results
+        // 4. Open disassembler report and compare machine code
+
+        [Benchmark(Baseline = true)]
+        [ArgumentsSource(nameof(Data))]
+        public ulong Recursive(ulong n)
+        {
+            if (n == 1 || n == 2) return 1;
+            return Recursive(n - 2) + Recursive(n - 1);
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Data))]
+        public ulong RecursiveWithMemoization(ulong n)
+        {
+            return 0;
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Data))]
+        public ulong Iterative(ulong n)
+        {
+            return 0;
+        }
+
+        public IEnumerable<ulong> Data()
+        {
+            yield return 15;
+            yield return 35;
+        }
+    }
+}
