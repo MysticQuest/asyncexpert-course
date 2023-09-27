7a19bd424a2df6500d8a0bc34435d6d166b2d624
Mystic Quest <kirimns@gmail.com>
2023-09-28 02:56:38 +0300

    adds exercise, main

--- Threading/ThreadPoolExercises/Program.cs
+++ Threading/ThreadPoolExercises/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+using ThreadPoolExercises.Core;
+
+namespace ThreadPoolExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Here you can play around with those method, prototype and easily debug
+
+            Console.WriteLine($"Main thread is {Thread.CurrentThread.ManagedThreadId}");
+
+            ThreadingHelpers.ExecuteOnThread(() =>
+            {
+                var thread = Thread.CurrentThread;
+                Console.WriteLine($"Hello from thread {thread.ManagedThreadId} from a pool: {thread.IsThreadPoolThread}");
+            }, 3);
+
+            ThreadingHelpers.ExecuteOnThreadPool(() =>
+            {
+                var thread = Thread.CurrentThread;
+                Console.WriteLine(
+                    $"Hello from thread {thread.ManagedThreadId} from a pool: {thread.IsThreadPoolThread}");
+            }, 3);
+        }
+    }
+}
