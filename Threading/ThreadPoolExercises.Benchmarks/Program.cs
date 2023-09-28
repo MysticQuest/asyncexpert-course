faf1e24b2b7a3a258d8e95db20528dda7355b862
Mystic Quest <kirimns@gmail.com>
2023-09-28 03:04:10 +0300

    adds hashes for benchmark

--- Threading/ThreadPoolExercises.Benchmarks/Program.cs
+++ Threading/ThreadPoolExercises.Benchmarks/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace ThreadPoolExercises.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ThreadingHelpersBenchmarks>();
+        }
+    }
+}
