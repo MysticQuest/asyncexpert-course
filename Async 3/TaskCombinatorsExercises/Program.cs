1c0e1bdcd491637df59945ee2380ed8c72e0a9c9
Mystic Quest <kirimns@gmail.com>
2023-10-05 19:27:45 +0300

    adds another operation

--- Async 3/TaskCombinatorsExercises/Program.cs
+++ Async 3/TaskCombinatorsExercises/Program.cs
@@ -8,6 +8,8 @@ namespace TaskCombinatorsExercises
 {
     class Program
     {
+        private static int timer = 3_000;
+
         static async Task Main(string[] args)
         {
             using var client = new HttpClient();
@@ -17,20 +19,33 @@ namespace TaskCombinatorsExercises
             {
                 var result = await client.ConcurrentDownloadAsync(new[]
                 {
-            "https://postman-echo.com/delay/3",
             "https://postman-echo.com/delay/4",
             "https://postman-echo.com/delay/5",
             "https://postman-echo.com/delay/6",
             "https://postman-echo.com/delay/7",
             "https://postman-echo.com/delay/8",
             "https://postman-echo.com/delay/9",
-            "https://postman-echo.com/delay/2",
-            }, 1_000, CancellationToken.None);
-                Debug(result);
+            "https://postman-echo.com/delay/10",
+            }, timer, CancellationToken.None);
+                Debug($"Operation with timeout: {timer} seconds, finished task {result}");
             }
             catch (TimeoutException ex)
             {
-                Debug($"Operation timed out: {ex.Message}");
+                Debug($"Operation with timeout: {timer} seconds, timed out: {ex.Message}");
+            }
+
+            try
+            {
+                var result = await client.ConcurrentDownloadAsync(new[]
+                {
+            "https://postman-echo.com/delay/1",
+            "https://postman-echo.com/delay/2",
+            }, timer, CancellationToken.None);
+                Debug($"Operation with timeout: {timer} seconds, finished task {result}");
+            }
+            catch (TimeoutException ex)
+            {
+                Debug($"Operation with timeout: {timer} seconds, timed out: {ex.Message}");
             }
         }
 
