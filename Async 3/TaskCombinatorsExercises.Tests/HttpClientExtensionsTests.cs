f156edd9cc041755bf5cad6cecb85c3c5ee506a9
Mystic Quest <kirimns@gmail.com>
2023-10-05 17:53:14 +0300

    sets up projects

---
+++ Async 3/TaskCombinatorsExercises.Tests/HttpClientExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using RichardSzalay.MockHttp;
+using TaskCombinatorsExercises.Core;
+using Xunit;
+
+namespace TaskCombinatorsExercises.Tests
+{
+    public class HttpClientExtensionsTests
+    {
+        [Fact]
+        public async Task GivenSingleCall_ThenSucceeds()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+            var mockedRequest1 = GivenDelayUrl(mockHttp, 700);
+
+            var result = await mockHttp.ToHttpClient().ConcurrentDownloadAsync(new[]
+            {
+                "https://local/delay/700"
+            }, 10_000, CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.Equal("700", result);
+            Assert.Equal(1, mockHttp.GetMatchCount(mockedRequest1));
+        }
+    }
+}
