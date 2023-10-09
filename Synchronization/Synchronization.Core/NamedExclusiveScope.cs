7743f6fe111188185bef7fa71c745d5a43ae8197
Mystic Quest <kirimns@gmail.com>
2023-10-09 22:47:13 +0300

    sets up solution and dependencies

--- Synchronization/Synchronization.Core/NamedExclusiveScope.cs
+++ Synchronization/Synchronization.Core/NamedExclusiveScope.cs
@@ -1,7 +1,20 @@
-﻿namespace Synchronization.Core
-{
-    public class NamedExclusiveScope
-    {
+﻿using System;
+using System.Threading;
 
+namespace Synchronization.Core
+{
+    /*
+     * Implement very simple wrapper around Mutex or Semaphore (remember both implement WaitHandle) to
+     * provide a exclusive region created by `using` clause.
+     *
+     * Created region may be system-wide or not, depending on the constructor parameter.
+     *
+     * Any try to get a second systemwide scope should throw an `System.InvalidOperationException` with `Unable to get a global lock {name}.`
+     */
+    public class NamedExclusiveScope : IDisposable
+    {
+        public NamedExclusiveScope(string name, bool isSystemWide)
+        {
+        }
     }
 }
--- Synchronization/Synchronization/Program.cs
+++ Synchronization/Synchronization/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+using Synchronization.Core;
+
+namespace Synchronization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scopeName = "default";
+            var isSystemWide = false;
+            if (args.Length == 2)
+            {
+                scopeName = args[0];
+                isSystemWide = bool.Parse(args[1]);
+            }
+            using (new NamedExclusiveScope(scopeName, isSystemWide))
+            {
+                Console.WriteLine("Hello world!");
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
