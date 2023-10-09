3fe919052b312048960287d4e30ec736df4893ca
Mystic Quest <kirimns@gmail.com>
2023-10-10 02:58:16 +0300

    fixes logic

--- Synchronization/Synchronization.Core/NamedExclusiveScope.cs
+++ Synchronization/Synchronization.Core/NamedExclusiveScope.cs
@@ -3,49 +3,37 @@ using System.Threading;
 
 namespace Synchronization.Core
 {
-    /*
-     * Implement very simple wrapper around Mutex or Semaphore (remember both implement WaitHandle) to
-     * provide a exclusive region created by `using` clause.
-     *
-     * Created region may be system-wide or not, depending on the constructor parameter.
-     *
-     * Any try to get a second systemwide scope should throw an `System.InvalidOperationException` 
-     * with `Unable to get a global lock {name}.`
-     */
     public class NamedExclusiveScope : IDisposable
     {
-        Semaphore semaphore;
+        private readonly Semaphore semaphore;
+        private bool hasHandle = false;
+
         public NamedExclusiveScope(string name, bool isSystemWide)
         {
-            if (isSystemWide == false)
+            if (isSystemWide)
             {
-                semaphore = new Semaphore(1, 1);
-                semaphore.WaitOne();
+                semaphore = new Semaphore(1, 1, name, out bool createdNew);
+                hasHandle = semaphore.WaitOne(0);
+
+                if (!createdNew && !hasHandle)
+                {
+                    throw new InvalidOperationException($"Unable to get a global lock {name}.");
+                }
             }
             else
             {
-                semaphore = new Semaphore(1, 1, name);
-                semaphore.WaitOne();
-            }
-            bool acquired = semaphore.WaitOne(0);
-            if (!acquired)
-            {
-                throw new InvalidOperationException($"Unable to get a global lock {name}.");
+                semaphore = new Semaphore(1, 1);
+                hasHandle = semaphore.WaitOne(0);
             }
         }
 
         public void Dispose()
         {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
+            if (hasHandle)
             {
                 semaphore.Release();
             }
+            semaphore.Dispose();
         }
     }
 }
